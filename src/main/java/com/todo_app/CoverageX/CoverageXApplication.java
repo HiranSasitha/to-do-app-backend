0dce7af589c3e8a5beace10d52a27f95760e1d12
HiranSasitha <hiran_h@epiclanka.net>
2025-10-23 10:28:41 +0530

    first commit

---
+++ src/main/java/com/todo_app/CoverageX/CoverageXApplication.java
@@ -0,0 +1,13 @@
+package com.todo_app.CoverageX;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoverageXApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoverageXApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/todo_app/CoverageX/CoverageXApplicationTests.java
@@ -0,0 +1,13 @@
+package com.todo_app.CoverageX;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoverageXApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
